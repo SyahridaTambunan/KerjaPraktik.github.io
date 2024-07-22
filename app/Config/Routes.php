2358d9a285e84459f09245627bfefd4cc88cf825
kevinnapit <kevinphy92@gmail.com>
2024-07-22 15:59:00 +0700

    fix error

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,17 +17,12 @@ $routes->post('/registercontroller/store', 'RegisterController::store');
 $routes->get('items', 'ItemController::index');
 $routes->get('inventaris/edit/(:num)', 'Inventaris::edit/$1');
 $routes->post('inventaris/inventaris_update/(:num)', 'Inventaris::inventaris_update/$1');
-
-
 $routes->get('inventaris/delete/(:segment)', 'inventaris::delete/$1');
-<<<<<<< HEAD
+
+$routes->get('categoris/create', 'Categoris::create');
+
 
 //location
 $routes->get('location/create', 'Location::create');
 $routes->post('location/location_store', 'Location::location_store');
 $routes->post('location/delete', 'Location::delete');
-=======
-$routes->get('inventaris/edit/(:num)', 'Inventaris::edit/$1');
-$routes->post('inventaris/update/(:num)', 'Inventaris::update/$1');
-$routes->get('location/delete/(:num)', 'Location::delete/$1');
->>>>>>> f0739b35dbdaa6d4faa7fa21a683f14dbc1d5754
